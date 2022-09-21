34422b8469355da5ce2041f2b46c6b03182abfc1
tinakw <110048198+tinakw@users.noreply.github.com>
2022-09-20 22:52:25 -0400

    still updating

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 // const veggies = require('./models/veggies');
 require('dotenv').config();
 const mongoose = require('mongoose');
+const Log = require('./models/logs');
 // const Fruit = require('./models/Fruit');
 
 // -------------------------
@@ -45,10 +46,41 @@ app.use(express.urlencoded({extended:false}));
 // -------------------------
 // Routes
 // -------------------------
+app.get('/', (req, res) => {
+  res.render('Index') 
+ });
 
-const flightsController = require('./controllers/flights');
-app.use('/flights', flightsController);
-// the app is using the route of /flights for the requests inside the flightsController
+ app.get('/create', (req, res) => {
+  res.render('received') 
+ });
+
+ // -------------------------
+// Create
+// -------------------------
+
+ app.post('/create', (req, res)=>{
+  if(req.body.shipisbroken === 'on'){
+    req.body.shipisbroken = true;
+  } else{
+    req.body.shipisbroken = false;
+  }
+  Log.create(req.body, (error, createdLog)=>{
+    res.redirect('/logs')
+  })
+ })
+
+ // -------------------------
+// Show
+// -------------------------
+
+app.get('/logs/:id', (req, res)=>{
+  Logs.findOne({_id: req.params.id},(error, foundLog)=>{
+    res.render('logs/Show',{
+      log: foundLog
+    });
+  });
+});
+
 
 // -------------------------
 // App Is Listening Thing
